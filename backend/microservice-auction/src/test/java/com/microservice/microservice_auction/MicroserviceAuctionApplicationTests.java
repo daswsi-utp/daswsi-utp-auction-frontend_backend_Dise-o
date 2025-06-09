f886a7bc0b36dd64f276247ece511db45997f0d1
Sogni_Appesi <juegosxepor@gmail.com>
2025-06-08 22:11:51 -0500

    Creating backend containers, configuring container inheritance to microservices

---
+++ backend/microservice-auction/src/test/java/com/microservice/microservice_auction/MicroserviceAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservice_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
