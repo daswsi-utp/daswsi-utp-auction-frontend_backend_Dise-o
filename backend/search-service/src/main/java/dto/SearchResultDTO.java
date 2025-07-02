0e63ca37d5a941e4c06fba04ac63d6e5fae208b6
jefferson124 <105380203+jefferson124@users.noreply.github.com>
2025-07-02 16:39:34 -0500

    Update SearchResultDTO.java

--- backend/search-service/src/main/java/dto/SearchResultDTO.java
+++ backend/search-service/src/main/java/dto/SearchResultDTO.java
@@ -1,1 +1,79 @@
+package dto;
+
+public class SearchResultDTO {
+    private Long auctionId;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private String category;
+    private Double basePrice;
+    private Double initialPrice;
+
+    public SearchResultDTO(Long auctionId, String name, String description, String imageUrl,
+                           String category, Double basePrice, Double initialPrice) {
+        this.auctionId = auctionId;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.basePrice = basePrice;
+        this.initialPrice = initialPrice;
+    }
+
+    public Long getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(Long auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(Double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public Double getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(Double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+}
 
