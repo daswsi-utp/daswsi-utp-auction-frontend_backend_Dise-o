b6f17d4a13212604df999f93fb72c5daeca778e1
kiliend <luis.condori.h98@gmail.com>
2025-07-02 17:42:12 -0500

    add configurations for DTO, and modificated congiguration from config server for auctions services

--- backend/microservice-auction/src/main/java/com/microservice/microservice_auction/controller/AuctionController.java
+++ backend/microservice-auction/src/main/java/com/microservice/microservice_auction/controller/AuctionController.java
@@ -8,6 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(
+  origins = "*",
+  allowedHeaders = "*",
+  methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS }
+)
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
