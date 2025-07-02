4105ccec5caa7e5a9e48781420bdb80f27a98fe2
jefferson124 <105380203+jefferson124@users.noreply.github.com>
2025-07-02 16:36:28 -0500

    Update AuctionDTO.java

--- backend/search-service/src/main/java/dto/AuctionDTO.java
+++ backend/search-service/src/main/java/dto/AuctionDTO.java
@@ -1,1 +1,31 @@
+package dto;
 
+public class AuctionDTO {
+    private Long id;
+    private Long productId;
+    private Double initialPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Double getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(Double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+}
