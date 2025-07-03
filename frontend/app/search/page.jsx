20f608c294fb601d93a10c3ee9b5cb96f6cfe00c
jefferson124 <105380203+jefferson124@users.noreply.github.com>
2025-07-03 16:53:26 -0500

    Update page.jsx

--- frontend/app/search/page.jsx
+++ frontend/app/search/page.jsx
@@ -2,39 +2,19 @@
 
 import React from 'react';
 import Header from '../componets/Header';
+import './search.css';
+
 export default function SearchPage() {
   return (
     <div>
-      <Header /> {/* ✅ Mantiene el navbar */}
-      <main style={styles.container}>
-        <h1 style={styles.title}>🔧 Página de busqueda en mantenimiento</h1>
-        <p style={styles.subtitle}>
+      <Header />
+      <main className="maintenance-container">
+        <h1 className="maintenance-title">🔧 Página en mantenimiento</h1>
+        <p className="maintenance-subtitle">
           Estamos trabajando para mejorar la experiencia de búsqueda.
         </p>
-        <p style={styles.subtitle}>Por favor, vuelve más tarde. 🙏</p>
+        <p className="maintenance-subtitle">Por favor, vuelve más tarde. 🙏</p>
       </main>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '80vh', 
-    backgroundColor: '#f7f7f7',
-    color: '#333',
-    textAlign: 'center',
-    padding: '2rem',
-  },
-  title: {
-    fontSize: '2.5rem',
-    marginBottom: '1rem',
-  },
-  subtitle: {
-    fontSize: '1.2rem',
-    marginBottom: '0.5rem',
-  },
-};
