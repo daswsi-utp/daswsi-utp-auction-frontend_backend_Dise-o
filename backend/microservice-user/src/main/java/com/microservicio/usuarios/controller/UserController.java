95d5c7f1ba90927118f1bfee8e9c2412caf0a989
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:41:29 -0500

    Improve user controller methods

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/controller/UserController.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.microservicio.usuarios.controller;
 
+import com.microservicio.usuarios.dto.UserRequest;
+import com.microservicio.usuarios.dto.UserResponse;
+import com.microservicio.usuarios.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.updateUser(id, userRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
