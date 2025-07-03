824ef34e2b0b9812dd45b6b6b51390c147b95afc
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:41:39 -0500

    Enhance UserResponse DTO formatting

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/UserResponse.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/UserResponse.java
@@ -1,4 +1,21 @@
 package com.microservicio.usuarios.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String country;
+    private String zipcode;
+    private String profileImage;
 }
