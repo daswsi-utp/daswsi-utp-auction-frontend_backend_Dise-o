a5bde75e17cade423708e9f259520318a1167839
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:42:01 -0500

    Update UserService interface

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/service/UserService.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/service/UserService.java
@@ -1,4 +1,14 @@
 package com.microservicio.usuarios.service;
 
-public interface UserService {
+import com.microservicio.usuarios.dto.UserRequest;
+import com.microservicio.usuarios.dto.UserResponse;
+import com.microservicio.usuarios.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    UserResponse registerUser(UserRequest userRequest);
+    UserResponse getUserById(Long id);
+    UserResponse updateUser(Long id, UserRequest userRequest);
+    void deleteUser(Long id);
+    User processOAuthPostLogin(String email, String name);
 }
