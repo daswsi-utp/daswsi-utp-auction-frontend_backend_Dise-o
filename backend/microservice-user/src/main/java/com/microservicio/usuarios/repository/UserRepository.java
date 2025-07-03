aded0e2546c32d056e0244d853454b0b75dadf24
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:41:51 -0500

    Update UserRepository interface logic

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/repository/UserRepository.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.microservicio.usuarios.repository;
 
-public interface UserRepository {
+import com.microservicio.usuarios.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
