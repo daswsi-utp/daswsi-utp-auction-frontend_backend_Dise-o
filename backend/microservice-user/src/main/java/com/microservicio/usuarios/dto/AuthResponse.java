b5eb2d837cdd7fcb41f66ec50e1df99e8eb8416f
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:41:33 -0500

    Refactor AuthResponse DTO structure

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/AuthResponse.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/AuthResponse.java
@@ -1,4 +1,14 @@
 package com.microservicio.usuarios.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
+    private String token;
 }
