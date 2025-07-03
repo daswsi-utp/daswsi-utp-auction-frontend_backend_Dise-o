848462b6cf605e46130d172c8b4b8d7244da4539
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-02 21:41:37 -0500

    Update UserRequest DTO content

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/UserRequest.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/dto/UserRequest.java
@@ -1,4 +1,20 @@
 package com.microservicio.usuarios.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String country;
+    private String zipcode;
 }
