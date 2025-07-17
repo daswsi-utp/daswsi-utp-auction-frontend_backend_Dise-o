704797952686fb3ce16cc26df8cfa419d780db02
huahuaccapa <huahuaccapa1119@gmail.com>
2025-07-17 01:27:39 -0500

    Updated UsuariosApplication.java file

--- backend/microservice-user/src/main/java/com/microservicio/usuarios/UsuariosApplication.java
+++ backend/microservice-user/src/main/java/com/microservicio/usuarios/UsuariosApplication.java
@@ -2,11 +2,13 @@ package com.microservicio.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-
 public class UsuariosApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(UsuariosApplication.class, args);
     }
 }
